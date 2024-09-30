687439cb3301dbe60acaf04b994335d8dc299e75
linnykoleh <oleh.linnyk@megogo.net>
2024-09-30 11:21:51 +0300

    initial commit

--- src/main/java/demo/bsrcountingsortdemo/BsrCountingSortDemoApplication.java
+++ src/main/java/demo/bsrcountingsortdemo/BsrCountingSortDemoApplication.java
@@ -1,13 +1,34 @@
 package demo.bsrcountingsortdemo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Random;
 
-@SpringBootApplication
 public class BsrCountingSortDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BsrCountingSortDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        var tree = new BalancedBinarySearch();
+        var random = new Random();
+
+        for (var i = 0; i < 100; i++) {
+            var dataset = new int[1000];
+            for (var j = 0; j < dataset.length; j++) {
+                dataset[j] = random.nextInt(10000);
+            }
+
+            // Measure time to insert
+            var startTime = System.nanoTime();
+            for (var value : dataset) {
+                tree.root = tree.insert(tree.root, value);
+            }
+            var endTime = System.nanoTime();
+            System.out.println("Insertion time for dataset " + i + ": " + (endTime - startTime) + " ns");
+
+            startTime = System.nanoTime();
+            for (int value : dataset) {
+                tree.root = tree.delete(tree.root, value);
+            }
+            endTime = System.nanoTime();
+            System.out.println("Deletion time for dataset " + i + ": " + (endTime - startTime) + " ns");
+        }
+    }
 }
--- src/main/java/demo/bsrcountingsortdemo/CountingSortAlgorithm.java
+++ src/main/java/demo/bsrcountingsortdemo/CountingSortAlgorithm.java
@@ -1,4 +1,49 @@
 package demo.bsrcountingsortdemo;
 
 public class CountingSortAlgorithm {
+
+    private static void countingSort(int[] arr) {
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+
+        // Find the maximum element
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        int[] count = new int[max + 1];
+
+        for (int j : arr) {
+            count[j]++;
+        }
+
+        for (int i = 1; i <= max; i++) {
+            count[i] += count[i - 1];
+        }
+
+        int[] output = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[arr[i]] - 1] = arr[i];
+            count[arr[i]]--;
+        }
+
+        // Copy the sorted elements into the original array
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 2, 8, 3, 3, 1};
+        countingSort(arr);
+
+        System.out.print("Sorted array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
 }
